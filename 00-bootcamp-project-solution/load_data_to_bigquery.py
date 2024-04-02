5330fc6c417bfde7bdcdf43d0f1078787b8e85e3
Firstnapat <firstnapat@odds.team>
2024-04-02 23:10:16 +0700

    feat: add solution before refactor code

--- 00-bootcamp-project-solution/load_data_to_bigquery.py
+++ 00-bootcamp-project-solution/load_data_to_bigquery.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 from google.cloud import bigquery
 from google.oauth2 import service_account
@@ -20,33 +19,6 @@ client = bigquery.Client(
     credentials=credentials,
 )
 
-def load_data_with_partition(data, dt):
-    job_config = bigquery.LoadJobConfig(
-        skip_leading_rows=1,
-        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-        source_format=bigquery.SourceFormat.CSV,
-        autodetect=True,
-        time_partitioning=bigquery.TimePartitioning(
-            type_=bigquery.TimePartitioningType.DAY,
-            field="created_at",
-        ),
-    )
-
-    partition = dt.replace("-", "")
-    file_path = f"{DATA_FOLDER}/{data}.csv"
-    with open(file_path, "rb") as f:
-        table_id = f"{project_id}.dbi.{data}${partition}"
-        job = client.load_table_from_file(f, table_id, job_config=job_config)
-        job.result()
-
-    table = client.get_table(table_id)
-    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
-
-load_data_with_partition("events", "2021-02-10")
-load_data_with_partition("orders", "2021-02-10")
-load_data_with_partition("users", "2021-02-23")
-
-
 def load_data_without_partition(data):
     job_config = bigquery.LoadJobConfig(
         skip_leading_rows=1,
@@ -63,13 +35,54 @@ def load_data_without_partition(data):
     table = client.get_table(table_id)
     print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
 
+
+def load_data_with_partition(data, dt, clustering_fields=[]):
+    if clustering_fields:
+        job_config = bigquery.LoadJobConfig(
+            skip_leading_rows=1,
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+            source_format=bigquery.SourceFormat.CSV,
+            autodetect=True,
+            time_partitioning=bigquery.TimePartitioning(
+                type_=bigquery.TimePartitioningType.DAY,
+                field="created_at",
+            ),
+            clustering_fields=clustering_fields,
+        )
+    else:
+        job_config = bigquery.LoadJobConfig(
+            skip_leading_rows=1,
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+            source_format=bigquery.SourceFormat.CSV,
+            autodetect=True,
+            time_partitioning=bigquery.TimePartitioning(
+                type_=bigquery.TimePartitioningType.DAY,
+                field="created_at",
+            ),
+        )
+
+    partition = dt.replace("-", "")
+    file_path = f"{DATA_FOLDER}/{data}.csv"
+    with open(file_path, "rb") as f:
+        table_id = f"{project_id}.dbi.{data}${partition}"
+        job = client.load_table_from_file(f, table_id, job_config=job_config)
+        job.result()
+
+    table = client.get_table(table_id)
+    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+
+
+# load_data_with_partition("events", "2021-02-10")
+# load_data_with_partition("orders", "2021-02-10")
+    
+load_data_with_partition("users", "2020-10-23", ["first_name", "last_name"])
+
 data = [
     "addresses",
     "promos",
     "products",
     "order_items",
 ]
-
 
 for each in data:
     load_data_without_partition(each)
